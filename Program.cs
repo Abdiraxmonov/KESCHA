81fcf2263e3e56ac6ff910f11255fb5c97b4c483
Abror Abdiraxmonov <103137972+Abdiraxmonov@users.noreply.github.com>
2023-04-26 13:52:16 +0500

    USID:a

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Clear();
+
+System.Console.WriteLine(sizeof(char));
+char smallA = 'a';
+char caoitalA = 'A';
+Console.WriteLine((int)smallA);
+Console.WriteLine((int)caoitalA);
+
+// string ism = "Abror";           // " " string un 2 tirnoq ishlatiladi
+// System.Console.WriteLine(ism);
+// string ism2 = 'Abror2';         // ' ' xato 1 tirnoq
+// System.Console.WriteLine(ism2);
+
+// char ism3 = 'A';                  // ' ' va 1ta belgi buladi
+// System.Console.WriteLine(ism3);
+// char ism4 = "A";                  // " "xato
+// System.Console.WriteLine(ism3);
+// char ism5 ='ab';                  // 'ab' xato 1ta belgidan kop buldi
+// System.Console.WriteLine(ism3);
